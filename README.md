77170b6940af283a781a07d0642e5f62df7ab367
Vladislav Osin <quakumei@gmail.com>
2022-06-11 20:27:56 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# powmobile
+ PowMobile GitHub
---
+++ android/app/src/main/kotlin/site/thevladoss/pow/powmobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package site.thevladoss.pow.powmobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
